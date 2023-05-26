546b4af2490d057dbc60f904197b093a4538a7ca
evgeniy-strel <evgeniy.strel@yandex.ru>
2023-05-26 15:40:09 +0500

    commit

---
+++ Migrations/20230526085655_initial-migrations.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Project1.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialmigrations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Person",
+                columns: table => new
+                {
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    Password = table.Column<string>(type: "text", nullable: false),
+                    Role = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Person", x => x.Email);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Subjects",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Semester = table.Column<List<int>>(type: "integer[]", nullable: false),
+                    SubjectName = table.Column<string>(type: "text", nullable: false),
+                    AddedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    b64Image = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Subjects", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tracks",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    TrackName = table.Column<string>(type: "text", nullable: true),
+                    SubjectId = table.Column<Guid>(type: "uuid", nullable: false),
+                    AddedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    b64Image = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tracks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Tracks_Subjects_SubjectId",
+                        column: x => x.SubjectId,
+                        principalTable: "Subjects",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Prepods",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    TrackId = table.Column<Guid>(type: "uuid", nullable: false),
+                    PrepodName = table.Column<string>(type: "text", nullable: false),
+                    Values_countReviews = table.Column<int>(type: "integer", nullable: false),
+                    Values_count1 = table.Column<int>(type: "integer", nullable: false),
+                    Values_count2 = table.Column<int>(type: "integer", nullable: false),
+                    Values_count3 = table.Column<int>(type: "integer", nullable: false),
+                    Values_count4 = table.Column<int>(type: "integer", nullable: false),
+                    Values_count5 = table.Column<int>(type: "integer", nullable: false),
+                    Values_avgRating = table.Column<double>(type: "double precision", nullable: false),
+                    Values_avgInterest = table.Column<double>(type: "double precision", nullable: false),
+                    Values_avgBenefit = table.Column<double>(type: "double precision", nullable: false),
+                    Values_avgAvailability = table.Column<double>(type: "double precision", nullable: false),
+                    AddedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    b64Image = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Prepods", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Prepods_Tracks_TrackId",
+                        column: x => x.TrackId,
+                        principalTable: "Tracks",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Reviews",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    PrepodId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Body = table.Column<string>(type: "text", nullable: false),
+                    AdditionalImages = table.Column<List<string>>(type: "text[]", nullable: false),
+                    Rating = table.Column<int>(type: "integer", nullable: false),
+                    Interest = table.Column<int>(type: "integer", nullable: false),
+                    Benefit = table.Column<int>(type: "integer", nullable: false),
+                    Availability = table.Column<int>(type: "integer", nullable: false),
+                    isMoved = table.Column<bool>(type: "boolean", nullable: false),
+                    AddedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    b64Image = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reviews", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Reviews_Prepods_PrepodId",
+                        column: x => x.PrepodId,
+                        principalTable: "Prepods",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Prepods_TrackId",
+                table: "Prepods",
+                column: "TrackId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_PrepodId",
+                table: "Reviews",
+                column: "PrepodId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tracks_SubjectId",
+                table: "Tracks",
+                column: "SubjectId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Person");
+
+            migrationBuilder.DropTable(
+                name: "Reviews");
+
+            migrationBuilder.DropTable(
+                name: "Prepods");
+
+            migrationBuilder.DropTable(
+                name: "Tracks");
+
+            migrationBuilder.DropTable(
+                name: "Subjects");
+        }
+    }
+}
